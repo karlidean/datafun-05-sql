9dbc934cbaf00b971f136f35049d3db58f2775b6
Karli Dean <karlidean11@gmail.com>
2025-09-25 17:15:11 -0500

    Learned WHERE function in SQL.

---
+++ filter_where_records.sql
@@ -0,0 +1,8 @@
+-- Selecting books released after a certain year
+-- SELECT * FROM books WHERE year_published > 1950;
+
+-- You can also filter based on anything in the table you pull from
+-- The table you pull from determines what you can pull
+
+-- To avoid duplicates, use the DISTINCT function
+SELECT DISTINCT title FROM books WHERE year_published = 1997;
