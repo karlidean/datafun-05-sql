829e5a383db78d9c6142e8a3e03875f7c89bb5c7
Karli Dean <karlidean11@gmail.com>
2025-09-25 17:19:55 -0500

    Learned the ORDER BY function in SQL.

---
+++ order_by_records.sql
@@ -0,0 +1,5 @@
+-- Sorting the books by publishing year in ascending order
+SELECT * FROM books ORDER BY year_published;
+
+-- Sorting the authors by their last name Z-A
+SELECT * FROM authors ORDER BY last DESC;
