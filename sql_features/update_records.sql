2889ff747f04641285a8540618eeb5a8e54e541c
Karli Dean <karlidean11@gmail.com>
2025-09-25 18:00:52 -0500

    Learned UPDATE function in SQL

---
+++ sql_features/update_records.sql
@@ -0,0 +1,9 @@
+-- Update the genre of a movie
+UPDATE books
+SET title = 'The Hobbit'
+WHERE year_published = 1937;
+
+-- Correct the release year of a movie
+-- UPDATE books
+-- SET release_year = 2008
+-- WHERE title = 'Pulp Fiction';
