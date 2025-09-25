b0bc838dd23ca4ad3025a3d3c30b94db32979c0d
Karli Dean <karlidean11@gmail.com>
2025-09-25 17:07:13 -0500

    Learned SELECT function in SQL.

---
+++ selecting_records.sql
@@ -0,0 +1,8 @@
+-- Just uncomment the commands you want to use.
+
+-- Selecting from ALL of a table (authors, books, etc)
+-- SELECT * FROM authors;
+-- The asterisk selects ALL records from the specified table
+
+-- Selecting specific fields from the books table
+ SELECT title, author_id FROM books;
